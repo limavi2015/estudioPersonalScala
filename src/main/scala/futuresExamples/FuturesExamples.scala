c557690a47b67a340165866285efbec89ee149e2
lilianamanjarres <limavi2007@hotmail.com>
2017-03-21 14:29:37 -0500

    modificacion

--- src/main/scala/futuresExamples/FuturesExamples.scala
+++ src/main/scala/futuresExamples/FuturesExamples.scala
@@ -7,28 +7,56 @@ import ExecutionContext.Implicits.global
 import scala.util.{Success, Failure, Try}
 
 class FuturesExamples {
-  val futuro1: Future[String] = Future{
+  val futuro1String: Future[String] = Future{
     Thread.sleep(10000)
     "pink"
   }
 
+  val futuro2Num: Future[Int] = Future{
+    Thread.sleep(10000)
+    3
+  }
+
   def runFutures={
     println("*** runFutures ***")
-    futuroNoBloqueante
-    futuroBloqueante
+    //futuroBloqueante
+    //futuroNoBloqueante
+    forComprehensions
+    Thread.sleep(12000)
+    println("*** termino la ejecucion runFutures ***")
   }
 
   private def futuroNoBloqueante={
     println("** Futuro No Bloqueante")
-    futuro1 onComplete {
-      case Success(result) =>println("resultado del onComplete:" + result)
-      case Failure(e) => println("Error: " + e.getMessage)
+    futuro1String onComplete {
+      case Success(result) =>println("OnComplete Success:" + result)
+      case Failure(e) => println("OnComplete Failure: " + e.getMessage)
     }
     println("El programa continua en ejecución")
   }
 
   private def futuroBloqueante={
     println("** Futuro Bloqueante")
-    println("resultado del Await: " + Await.result(futuro1,12.seconds))
+    println("resultado del Await: " + Await.result(futuro1String,12.seconds))
+    println("El programa llega a este punto cuando el futuro bloqueante ha terminado")
+  }
+
+  private def forComprehensions={
+    println("** for Comprehensions, tampoco es bloqueante")
+   for {
+      f1 <- futuro1String
+      f2 <- futuro2Num
+   }yield (println("**El resultado del for Comprehensions: "+ "f1:" +f1 +", f2:"+ f2))
+    println("El programa continua en ejecución 2")
   }
 }
+
+
+
+/*Para ejecutar futuros de la librería estándar se requiere tener un ExecutionContext, que es algo que ejecuta
+código típicamente en un pool de threads.s de la librería estándar se requiere tener un ExecutionContext,
+que es algo que ejecuta código típicamente en un pool de threads.
+
+Scala provee un contexto de ejecución global (que se utiliza haciendo import scala.concurrent.ExecutionContext.
+Implicits.global), pero para aplicaciones de verdad se sugiere tener varios aislados.
+ */
